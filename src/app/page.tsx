e92cab91a8159ad32f5b297c325a8feabff21f9c
pheralb <pablohdez4340@gmail.com>
2023-05-17 11:26:31 +0100

    Initial externalLink component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+import { getOffers } from "@/server/services/getOffers";
+import { ExternalLink } from "@/ui/link";
+
 export default function Home() {
+  // const listOfOffers = await getOffers();
+  // console.log(listOfOffers);
   return (
     <>
       <h1>Hello World 🚀</h1>
+      <ExternalLink
+        href="https://nextjs.org"
+        external={true}
+        externalIcon={true}
+      >
+        Next.js!
+      </ExternalLink>
     </>
-  )
+  );
 }
