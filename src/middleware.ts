2def8c72dca675c33e3b957645ad38214e0f10c0
pheralb <pablohdez4340@gmail.com>
2023-05-19 11:30:09 +0100

    Initial middleware config with next-auth.

---
+++ src/middleware.ts
@@ -0,0 +1,39 @@
+import { getToken } from "next-auth/jwt";
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export default withAuth(
+  async function middleware(req) {
+    const token = await getToken({ req });
+    const isAuth = !!token;
+    const isAuthPage = req.nextUrl.pathname.startsWith("/auth");
+
+    if (isAuthPage) {
+      if (isAuth) {
+        return NextResponse.redirect(new URL("/profile", req.url));
+      }
+      return null;
+    }
+
+    if (!isAuth) {
+      let from = req.nextUrl.pathname;
+      if (req.nextUrl.search) {
+        from += req.nextUrl.search;
+      }
+      return NextResponse.redirect(
+        new URL(`/auth?from=${encodeURIComponent(from)}`, req.url)
+      );
+    }
+  },
+  {
+    callbacks: {
+      authorized() {
+        return true;
+      },
+    },
+  }
+);
+
+export const config = {
+  matcher: ["/profile/:path*", "/auth"],
+};
