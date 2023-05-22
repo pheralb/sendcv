4101a7e268c0d56ebb94c06b3661b931feaf8464
pheralb <pablohdez4340@gmail.com>
2023-05-22 10:45:27 +0100

    Design improvements.

--- src/ui/tooltip.tsx
+++ src/ui/tooltip.tsx
@@ -18,7 +18,7 @@ const TooltipContent = React.forwardRef<
     ref={ref}
     sideOffset={sideOffset}
     className={cn(
-      "z-50 overflow-hidden rounded-md bg-neutral-300 dark:bg-neutral-900 px-3 py-1.5 animate-in fade-in-50",
+      "z-50 overflow-hidden rounded-md bg-neutral-300 dark:bg-neutral-800 px-3 py-1.5 animate-in fade-in-50",
       "data-[side=top]:slide-in-from-bottom-1",
       "data-[side=bottom]:slide-in-from-top-1",
       "data-[side=left]:slide-in-from-right-0.5",
