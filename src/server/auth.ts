76934cf59357011729952e7cfae69a3180dd9f3b
pheralb <pablohdez4340@gmail.com>
2023-05-22 10:40:27 +0100

    Update auth config & add custom next-auth types.

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,29 +1,81 @@
-import lucia from "lucia-auth";
-import { nextjs } from "lucia-auth/middleware";
+import { type GetServerSidePropsContext } from "next";
+import { getServerSession, type NextAuthOptions } from "next-auth";
 import { env } from "@/env.mjs";
+import { prisma } from "@/server/db";
 
 // Adapter:
-import prisma from "@lucia-auth/adapter-prisma";
-import { PrismaClient } from "@prisma/client";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 // Providers:
-import { github } from "@lucia-auth/oauth/providers";
+import GithubProvider from "next-auth/providers/github";
 
-export const auth = lucia({
-  adapter: prisma(new PrismaClient()),
-  env: process.env.NODE_ENV === "development" ? "DEV" : "PROD",
-  middleware: nextjs(),
-  transformDatabaseUser: (userData) => {
-    return {
-      userId: userData.id,
-      username: userData.username,
-    };
+export const authOptions: NextAuthOptions = {
+  // Callbacks:
+  callbacks: {
+    async session({ token, session }) {
+      if (token) {
+        session.user.id = token.id;
+        session.user.name = token.name;
+        session.user.username = token.username;
+        session.user.email = token.email;
+        session.user.image = token.image;
+        session.user.description = token.description;
+        session.user.website = token.website;
+      }
+      return session;
+    },
+    async jwt({ token, user }) {
+      const dbUser = await prisma.user.findFirst({
+        where: {
+          email: token.email,
+        },
+      });
+      if (!dbUser) {
+        token.id = user?.id;
+        return token;
+      }
+      return {
+        id: dbUser.id,
+        name: dbUser.name,
+        username: dbUser.username,
+        email: dbUser.email,
+        image: dbUser.image,
+        description: dbUser.description,
+        website: dbUser.website,
+      };
+    },
   },
-});
+  // Providers:
+  providers: [
+    GithubProvider({
+      clientId: env.GITHUB_CLIENT_ID || "",
+      clientSecret: env.GITHUB_CLIENT_SECRET || "",
+      profile(profile) {
+        return {
+          id: profile.id.toString(),
+          name: profile.name || profile.login,
+          username: profile.login,
+          email: profile.email,
+          image: profile.avatar_url,
+        };
+      },
+    }),
+  ],
+  // Pages:
+  pages: {
+    signIn: "/auth",
+  },
+  // Session:
+  session: {
+    strategy: "jwt",
+  },
+  // Database:
+  adapter: PrismaAdapter(prisma),
+};
 
-export const githubAuth = github(auth, {
-  clientId: env.GITHUB_CLIENT_ID ?? "",
-  clientSecret: env.GITHUB_CLIENT_SECRET ?? "",
-});
-
-export type Auth = typeof auth;
+export const getServerAuthSession = (ctx: {
+  req: GetServerSidePropsContext["req"];
+  res: GetServerSidePropsContext["res"];
+}) => {
+  return getServerSession(ctx.req, ctx.res, authOptions);
+};
