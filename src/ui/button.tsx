6dffab378f15be4124ec83eec71adc452ce713ae
pheralb <pablohdez4340@gmail.com>
2023-05-22 11:05:46 +0100

    Fix button props + add custom spinner.

--- src/ui/button.tsx
+++ src/ui/button.tsx
@@ -1,5 +1,6 @@
 import clsx from "clsx";
 import type { ReactNode } from "react";
+import Spinner from "./spinner";
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
@@ -7,8 +8,8 @@ export interface ButtonProps
   children: ReactNode;
   onClick?: () => void | Promise<void>;
   icon?: ReactNode;
-  loadingStatus?: boolean;
-  loadingText?: string;
+  loadingstatus?: boolean;
+  loadingtext?: string;
   disabled?: boolean;
   wFull?: boolean;
 }
@@ -27,40 +28,24 @@ export const Button = (props: ButtonProps) => {
       type={props.type}
       className={clsx(
         ButtonStyles,
-        props.loadingStatus && "cursor-wait opacity-50",
+        props.loadingstatus && "cursor-wait opacity-50",
         props.disabled && "cursor-not-allowed opacity-50",
         props.wFull && "w-full"
       )}
       onClick={props.onClick}
-      disabled={props.disabled || props.loadingStatus}
+      disabled={props.disabled || props.loadingstatus}
       {...props}
     >
       <div className="flex items-center justify-center">
-        {props.loadingStatus && (
-          <svg
-            aria-hidden="true"
-            role="status"
-            className="mr-3 inline h-4 w-4 animate-spin text-white"
-            viewBox="0 0 100 101"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-              fill="#E5E7EB"
-            />
-            <path
-              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-              fill="currentColor"
-            />
-          </svg>
+        {props.loadingstatus && (
+          <Spinner className="mr-3 inline h-4 w-4 animate-spin text-white" />
         )}
-        {props.icon && !props.loadingStatus && (
+        {props.icon && !props.loadingstatus && (
           <span className="mr-3">{props.icon}</span>
         )}
         <span>
-          {props.loadingText && props.loadingStatus
-            ? props.loadingText
+          {props.loadingtext && props.loadingstatus
+            ? props.loadingtext
             : props.children}
         </span>
       </div>
