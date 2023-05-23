9b6e29c2d098df6e22aad0d3818a1dc578dba9a8
pheralb <pablohdez4340@gmail.com>
2023-05-23 15:28:08 +0100

    Update custom user types.

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,7 +1,13 @@
 export interface iUser {
+  id?: string;
   name: string;
   username: string;
+  email?: string;
+  emailVerified?: string;
+  verified: boolean;
   image: string;
   description: string;
   website: string;
+  linkedinUrl: string;
+  twitterUrl: string;
 }
