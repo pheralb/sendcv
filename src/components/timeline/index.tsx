78f8290fb87e218b57a52312a9628ee891da398b
pheralb <pablohdez4340@gmail.com>
2023-05-27 20:53:13 +0100

    Add deleteExperiencie + order experience by date.

--- src/components/timeline/index.tsx
+++ src/components/timeline/index.tsx
@@ -2,6 +2,7 @@ import { ExternalLink } from "@/ui/link";
 import type { ReactNode } from "react";
 
 import CreateUpdateExperience from "../profilePage/createUpdateExperience";
+import DeleteExperience from "../profilePage/deleteExperience";
 
 const TimelineProvider = ({ children }: { children: ReactNode }) => {
   return (
@@ -12,7 +13,9 @@ const TimelineProvider = ({ children }: { children: ReactNode }) => {
 };
 
 interface TimelineItemProps {
-  date?: Date | null;
+  id?: string;
+  startDate?: Date | null;
+  endDate?: Date | null;
   edit?: boolean;
   title: string;
   company?: string;
@@ -23,13 +26,41 @@ interface TimelineItemProps {
 const TimelineItem = (props: TimelineItemProps) => {
   return (
     <li className="ml-5 pt-1">
-      <div className="absolute -left-1.5 mt-1.5 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-neutral-900 dark:bg-neutral-700"></div>
+      <div className="absolute -left-1.5 mt-8 h-3 w-3 rounded-full border border-white bg-gray-200 dark:border-neutral-900 dark:bg-neutral-700"></div>
       <time className="mb-2 mt-3 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"></time>
       <div className="flex items-center justify-between">
-        <h3 className="text-lg text-neutral-900 dark:text-white">
-          {props.title}
-        </h3>
-        {props.edit && <CreateUpdateExperience status="edit" {...props} />}
+        <div className="flex flex-col space-y-2">
+          <div className="flex items-center space-x-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
+            {props.startDate && (
+              <time>
+                {props.startDate.toLocaleDateString("en-US", {
+                  year: "numeric",
+                  month: "long",
+                })}
+              </time>
+            )}
+            <span>-</span>
+            {props.endDate ? (
+              <time>
+                {props.endDate.toLocaleDateString("en-US", {
+                  year: "numeric",
+                  month: "long",
+                })}
+              </time>
+            ) : (
+              <span>Present</span>
+            )}
+          </div>
+          <h3 className="text-lg text-neutral-900 dark:text-white">
+            {props.title}
+          </h3>
+        </div>
+        {props.edit && (
+          <div className="flex items-center space-x-1">
+            <CreateUpdateExperience status="edit" {...props} />
+            <DeleteExperience id={props.id!} />
+          </div>
+        )}
       </div>
       <div className="flex items-center space-x-2">
         <ExternalLink
