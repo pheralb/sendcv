5a0d0dc90d0d9e1407f0d65581d901dabc399860
pheralb <pablohdez4340@gmail.com>
2023-05-29 14:37:33 +0100

    Add @tailwindcss/typography plugin.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -28,5 +28,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
 } satisfies Config;
